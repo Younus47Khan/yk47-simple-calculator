9531dce308be81aa27a071f4bb17413236782f18
Younus47Khan <mrykhan10@gmail.com>
2024-05-16 17:20:10 +0500

    Just Made A Little Change

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+const answer = await inquirer.prompt([
+    { message: "Enter Your First Value", type: "number", name: "FirstNumber" },
+    { message: "Enter Your Second Value", type: "number", name: "SecondNumber" },
+    {
+        message: "Select One Of The Operators To Operate:",
+        type: "list",
+        name: "Operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (answer.Operator === "Addition") {
+    console.log(answer.FirstNumber + answer.SecondNumber);
+}
+else if (answer.Operator === "Subtraction") {
+    console.log(answer.FirstNumber - answer.SecondNumber);
+}
+else if (answer.Operator === "Multiplication") {
+    console.log(answer.FirstNumber * answer.SecondNumber);
+}
+else if (answer.Operator === "Division") {
+    console.log(answer.FirstNumber / answer.SecondNumber);
+}
+else {
+    console.log("Select Valid Operator");
+}
+console.log("Congrats! This is your answer");
